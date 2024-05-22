69a22293c6846f77b690004e186900e53d3229f5
Adem Aldemir <adem.aldemir@outlook.com>
2024-05-22 15:35:04 +0300

    taxi-book-driver added

---
+++ taxi-book-driver/src/main/java/com/aldemir/kafkademos/taxibookdriver/TaxiBookDriverApplication.java
@@ -0,0 +1,13 @@
+package com.aldemir.kafkademos.taxibookdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiBookDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiBookDriverApplication.class, args);
+    }
+
+}
