b5f1045b05af7872542c7eeabe127ef926872b6a
Adem Aldemir <adem.aldemir@outlook.com>
2024-05-22 15:33:49 +0300

    taxi-book-passenger added

---
+++ taxi-book-passenger/src/main/java/com/aldemir/kafkademos/taxibookpassenger/TaxiBookPassengerApplication.java
@@ -0,0 +1,13 @@
+package com.aldemir.kafkademos.taxibookpassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiBookPassengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiBookPassengerApplication.class, args);
+	}
+
+}
